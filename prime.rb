a084c00f16d359367c2dbab561ec8e94c4def765
Dana Pughakoff <danainjax@gmail.com>
2020-08-06 21:43:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,16 @@
 def prime?(i)
-  prime?.each do |i|
-  if i % 2 !=0 && i!=2 
-   true
-  else
-  false
- end
- end
-end
+  def prime?(i)
 
+     start = 2
+		 
+		 if i > 1
+		 
+		 range = (start..i-1).to_a
+		 range.none? do |num_to_test|
+		 i % num_to_test == 0
+end
+     else false
+		 
+end
+end
  
