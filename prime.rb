95d9e7ff1ff5bbb779c25582302bc9f22d999e44
Dana Pughakoff <danainjax@gmail.com>
2020-08-06 21:34:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,9 @@
 def prime?(i)
-  #if i % 2 !=0 && i!=2 
-   # true
-#else
-#  false
- # end
-#end
-start = 2
-  if number > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
-                                # return true, then they are not divisible by the number and the number is prime
-      number % num_to_test == 0
-    end
+  if i % 2 !=0 && i!=2 
+   true
   else
-    false
-  end
+  false
+ end
 end
+
  
